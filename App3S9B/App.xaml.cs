b27ccb1abc922d59931e453f162cbb621688eaf5
Renato Toasa <renato_toasa@hotmail.com>
2021-10-22 20:50:33 -0500

    Add project files.

---
+++ App3S9B/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App3S9B
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            //Habilitar Navegacion entre ventanas
+            MainPage = new NavigationPage (new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App3S9B/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3S9B
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            // Almacenar los datos en variables
+            string usuario = txtUsuario.Text;
+            string contrasena = txtContrasena.Text;
+
+            //Navegando a la ventana dos.
+            await Navigation.PushAsync(new viewDos(usuario, contrasena));
+        }
+    }
+}
